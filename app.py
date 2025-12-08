ff495657016752344b2a56bc996896be4cc54228
PlnThmrs <pauline.thomieres@gmail.com>
2025-12-08 11:22:21 +0100

    Edit app.py fonction lancer_exercice

--- app.py
+++ app.py
@@ -41,17 +41,55 @@ def choisir_utilisateur():
     return um.creer_ou_charger_utilisateur(utilisateur_courant)
 
 
-def lancer_exercice(): 
+def lancer_exercice():
+    print("Génération d'un exercice")
+ 
+    themes = {
+        "1": "Fonctions",
+        "2": "Listes",
+        "3": "Dictionnaires",
+        "4": "POO",
+        "5": "Fichiers et Exceptions",
+    }
+ 
+    print("Choisissez un thème :")
+    for numero, nom in themes:
+        print(f"{numero}. {nom}")
+ 
+    choix_theme = input("Votre choix (1-5) : ")
+ 
+    if choix_theme not in themes:
+        print("Choix de thème invalide.")
+ 
+    niveaux = {
+        "1": "Débutant",
+        "2": "Intermédiaire",
+        "3": "Avancé",
+    }
+ 
+    print("Choisissez un niveau :")
+    for numero, nom in niveaux:
+        print(f"{numero}, {nom}")
+ 
+    choix_niveau = input("Votre choix (1-3) : ")
+ 
+    print(f"Génération d'un exercice niveau {niveaux[choix_niveau]} sur : {themes[choix_theme]}")
     """Gère le processus complet de génération, soumission et évaluation de l'exercice."""
-    #j'instancie ma classe GeminiClient du module core_ai sur la variable gemini_client avec en paramètre la clé API
-    # j'appelle la fonction generer_exercice du module core_ai: j'ai besoin d'un thème et d'un niveau
-    #je récupére une consigne et un résultat, j'affiche seulement la consigne
-    #je demande la réponse du user
-    #j'envoie cette réponse, la consigne et la réponse attendue à la fonction evaluer_code du module core_ai
-    #je récupère le score, le commentaire et la solution attendue et j'affiche
-    #j'envoie le nom, score et la consigne à la fonction mettre_a_jour_score(nom, points, details_exo) du module user_manager
-    #je sauvegarde grâce à la fonction
-
+    gemini_client=GeminiClient(api_key,model) #j'instancie ma classe GeminiClient du module core_ai sur la variable gemini_client avec en paramètre la clé API
+    reponse=gemini_client.generer_exercice(themes[choix_theme],niveaux[choix_niveau]) # j'appelle la fonction generer_exercice du module core_ai: j'ai besoin d'un thème et d'un niveau
+    consigne=reponse.get('consigne','Consigne non disponible') #pour récupérer la consigne
+    solution=reponse.get('solution','Solution non disponible') #pour récupérer la solution
+    print(consigne) #je récupére une consigne et une solution, j'affiche seulement la consigne
+    code_eleve=input("Entrez votre réponse :") #je demande la réponse du user
+    evaluation=gemini_client.evaluer_code(code_eleve, consigne, solution) #j'envoie cette réponse, la consigne et la réponse attendue à la fonction evaluer_code du module core_ai
+    points_eleve=evaluation.get('score','score non disponible')
+    commentaire=evaluation.get('evaluation','Evaluation non disponible')
+    print(points_eleve,"/10")
+    print(commentaire)
+    print("Solution attendue :")
+    print(solution) #je récupère le score, le commentaire et la solution attendue et j'affiche
+    um.mettre_a_jour_score(utilisateur_courant, points_eleve, consigne) #j'envoie le nom, score et la consigne à la fonction mettre_a_jour_score(nom, points, details_exo) du module user_manager
+    um.sauvegarder_utilisateurs(data) #je sauvegarde grâce à la fonction
 
 def main(): 
     """Fonction principale de l'application."""
